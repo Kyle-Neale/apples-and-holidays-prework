f7df827727dbcde752b89eb3fffbdac51525fb45
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-03 17:36:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,10 +72,11 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     
-    holiday.each do |holiday, supply|
-      if supply == "BBQ"
-      
-        bbq << holiday
+    holiday.each do |holiday, supplies|
+      supplies.each do |supply|
+        if supply == "BBQ"
+          bbq << holiday
+        end
       end
     end
   end
